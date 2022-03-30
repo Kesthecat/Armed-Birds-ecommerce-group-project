dc04293d30cd1974642c4032ba3176b69ec97558
Lina Hu <lina.l.hu@gmail.com>
2022-03-30 18:27:19 -0400

    basic app + globalstyles

---
+++ client/src/components/PageWrapper.js
@@ -0,0 +1,11 @@
+import styledComponents from "styled-components";
+
+//page wrapper with set margins and padding
+
+const PageWrapper = () => {
+    return <Wrapper></Wrapper>
+}
+
+const Wrapper = styled.div`
+`
+export default PageWrapper;
