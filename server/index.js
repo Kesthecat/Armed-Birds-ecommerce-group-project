2e0a62e3be372bbfaf2e20dc6308825cacd81f64
Gina Wong <twgwong@gmail.com>
2022-03-31 00:26:15 -0400

    endpoints

--- server/index.js
+++ server/index.js
@@ -1,29 +1,47 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { addOrder } = require("./handlers/addOrder");
+const { deleteOrder } = require("./handlers/deleteOrder");
+const { getCompanies } = require("./handlers/getCompanies");
+const { getCompanyById } = require("./handlers/getCompanyById");
+const { getItemById } = require("./handlers/getItemById");
+const { getItems } = require("./handlers/getItems");
 
 const PORT = 4000;
 
 express()
-  .use(function(req, res, next) {
+  .use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Methods',
-      'OPTIONS, HEAD, GET, PUT, POST, DELETE'
+      "Access-Control-Allow-Methods",
+      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
     );
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('tiny'))
-  .use(express.static('./server/assets'))
+  .use(morgan("tiny"))
+  .use(express.static("./server/assets"))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
-  .use('/', express.static(__dirname + '/'))
+  .use("/", express.static(__dirname + "/"))
 
-  // REST endpoints?
-  .get('/bacon', (req, res) => res.status(200).json('🥓'))
+  // REST endpoints ----------------------------------------------------------
+  .get("/get-items", getItems)
+  .get("/get-item/:id", getItemById)
+  .get("/get-companies", getCompanies)
+  .get("/get-company/:id", getCompanyById)
+
+  .post("/addOrder", addOrder)
+
+  .delete("/deleteOrder/:id", deleteOrder)
+  // delete one item in an order?
+
+  .put("/modify-order/:id")
+
+  // -------------------------------------------------------------------------
 
   .listen(PORT, () => console.info(`Listening on port ${PORT}`));
