f96b320c3925dbf08698148ecb983915f4dd0d66
Gina Wong <twgwong@gmail.com>
2022-03-31 00:30:34 -0400

    getCompaniesHandler

---
+++ server/handlers/getCompanies.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getCompanies = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const db = client.db("Ecommerce");
+
+  await client.connect();
+  const result = await db.collection("Companies").find().toArray();
+  console.log("result", result);
+
+  //handle error
+  !result
+    ? res.status(404).json({
+        status: 404,
+        data: null,
+        message: "not found",
+      })
+    : res.status(200).json({ status: 200, data: result, message: "success" });
+
+  client.close();
+};
+module.exports = { getCompanies };
