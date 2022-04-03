c9db60ebc1509822f1970941fe7bdbe1c348ba5c
Gina Wong <twgwong@gmail.com>
2022-04-03 01:52:01 -0400

    company name

--- client/src/components/ShopPage/Dropdown.js
+++ client/src/components/ShopPage/Dropdown.js
@@ -1,42 +1,42 @@
 import styled from "styled-components";
 
 //dropdown menu component, used for the quantity dropdown on the ItemDetails page
-const Dropdown = ({array, label, stateSetter}) => {
+const Dropdown = ({ array, label, stateSetter }) => {
+  const handleSelect = (e) => {
+    stateSetter(e.target.value);
+    console.log("selected qt", e.target.value);
+  };
 
-    const handleSelect = (e) => { 
-        stateSetter(e.target.value); 
-        console.log("selected qt", e.target.value)
-    }
-
-    return (
-        <Wrapper>
-            <label>{label}</label>
-            <select onChange={handleSelect} name="chooseQty" id="chooseQty">
-                <option selected disabled>Choose:</option>
-            
-                    {array.map((el) => {
-                        return <option value={el} key={el}>{el}</option>
-                    })
-
-                
-                }
-            </select>
-        </Wrapper>
-    )
-
-}
+  return (
+    <Wrapper>
+      <label>{label}</label>
+      <select onChange={handleSelect} name="chooseQty" id="chooseQty">
+        <option selected disabled value={"Choose"}>
+          Choose:
+        </option>
+        {array.map((el) => {
+          return (
+            <option value={el} key={el}>
+              {el}
+            </option>
+          );
+        })}
+      </select>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
-    font-family: var(--font-body);
-    font-size: 18px;
-    label {
-        margin-right: 20px;
-        padding-right: 20px;
-    }
+  font-family: var(--font-body);
+  font-size: 18px;
+  label {
+    margin-right: 20px;
+    padding-right: 20px;
+  }
 
-    select {
-        font-size: 18px;
-        width: 100px;
-    }
-`
+  select {
+    font-size: 18px;
+    width: 100px;
+  }
+`;
 export default Dropdown;
