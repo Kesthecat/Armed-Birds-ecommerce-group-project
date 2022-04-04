7a4ea5e32e0c319f5d99c8594e4e00d336012df5
Gina Wong <twgwong@gmail.com>
2022-04-04 13:34:55 -0400

    footer and landing styling

--- client/src/components/PageWrapper.js
+++ client/src/components/PageWrapper.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 
 //page wrapper with set margins and padding
 
-const PageWrapper = ({children}) => {
-    return <Wrapper>{children}</Wrapper>
-}
+const PageWrapper = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 50px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+`;
 export default PageWrapper;
