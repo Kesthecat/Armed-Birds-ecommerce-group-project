04bf6f923f283399e60cd85940a202ffa438fee9
Ineily <isaacrneily@gmail.com>
2022-04-04 00:56:33 -0300

    confirmation page

--- client/src/components/Order/OrderContext.js
+++ client/src/components/Order/OrderContext.js
@@ -11,136 +11,141 @@ export const OrderContext = createContext();
 // - `purchased`
 
 const initialState = {
-    status: "idle", 
-    error: null, 
-    itemsPurchased: [], //array of item ids, names, quantities and prices
-    //has shape [{_id, name, price, quantity}]
-}
+  status: "idle",
+  error: null,
+  itemsPurchased: [], //array of item ids, names, quantities and prices
+  //has shape [{_id, name, price, quantity}]
+};
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case ("begin-order-process"): {
-            return {
-                ...state, 
-                status: "items-selected", 
-                error: null, 
-                // itemsToPurchase: action.selectedItems, 
-            };
-        }
-        case ("cancel-order-process"): { //for CANCEL BUTTON on checkout page
-            return {
-                ...initialState
-            }
-        }
-        case ("order-processing"): { 
-            return {
-                ...state,
-                status: "awaiting-response",
-                itemsPurchased: [...action.itemsPurchased]
-            }
-            
-        }
-        case ("order-failure"): {
-            return {
-                ...state,
-                status: "error",
-                itemsPurchased: null,
-                error: action.error, 
-            }
-        }
-        case ("order-success"): { 
-            return {
-                ...state,
-                status: "purchased",
-                // itemsPurchased: action.selectedItems, 
-            }
-        }
-        case ("after-order-reset"): {
-            return {
-                ...initialState,
-            }
-        }
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case "begin-order-process": {
+      return {
+        ...state,
+        status: "items-selected",
+        error: null,
+        // itemsToPurchase: action.selectedItems,
+      };
     }
-}
-
-
-export const OrderContextProvider = ({children}) => {
-
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    //keep track of user's selected item ids, names, quantity and prices in the current session before purchase
-    //stored in session storage
-    //has shape [{_id, name, price, quantity, subtotal, imageSrc}]
-    const [selectedItems, setSelectedItems] = usePersistedState([], "current-cart");
-
-    //state to indicate whether to display Cart Modal or not
-    const [displayModal, setDisplayModal] = useState(false);
-
-    const [ lastOrder, setLastOrder ] = useState(null);
-
-    const beginOrderProcess = () => {
-        dispatch({
-            type: "begin-order-process", 
-            // selectedItems: selectedItems, 
-        });
-    };
-
-    const cancelOrderProcess = () => {
-        // setSelectedItems([]);
-        dispatch({
-            type: "cancel-order-process", 
-        })
-    };
-
-    const orderRequested = () => {
-        const newArr = [...selectedItems];
-        dispatch({
-            type: "order-processing",
-            itemsPurchased: newArr,
-        })
-    };
-
-    const orderFailure = (errorMessage) => {
-        dispatch({
-            type: "order-failure",
-            error: errorMessage,
-        })
+    case "cancel-order-process": {
+      //for CANCEL BUTTON on checkout page
+      return {
+        ...initialState,
+      };
     }
-
-    const orderSuccess = () => {
-
-       dispatch({
-            type: "order-success",
-       })
-      //empty the cart
-        setSelectedItems([]);
+    case "order-processing": {
+      return {
+        ...state,
+        status: "awaiting-response",
+        itemsPurchased: [...action.itemsPurchased],
+      };
     }
-
-    const afterPurchaseReset = () => {
-        dispatch({
-            type: "after-purchase-reset",
-        })
+    case "order-failure": {
+      return {
+        ...state,
+        status: "error",
+        itemsPurchased: [],
+        error: action.error,
+      };
     }
+    case "order-success": {
+      return {
+        ...state,
+        status: "purchased",
+        // itemsPurchased: action.selectedItems,
+      };
+    }
+    case "after-order-reset": {
+      return {
+        ...initialState,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
-    return (
-        <OrderContext.Provider
-            value={{
-                state, 
-                actions: { beginOrderProcess,
-                    cancelOrderProcess,
-                    orderRequested,
-                    orderFailure,
-                    orderSuccess,
-                    afterPurchaseReset,
-                },
-                selectedItems, setSelectedItems,
-                displayModal, setDisplayModal, 
-                lastOrder, setLastOrder
-            }}
-            >{children}
-        </OrderContext.Provider>
-    )
-}
+export const OrderContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //keep track of user's selected item ids, names, quantity and prices in the current session before purchase
+  //stored in session storage
+  //has shape [{_id, name, price, quantity, subtotal, imageSrc}]
+  const [selectedItems, setSelectedItems] = usePersistedState(
+    [],
+    "current-cart"
+  );
+
+  //state to indicate whether to display Cart Modal or not
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const [lastOrder, setLastOrder] = useState(null);
+
+  const beginOrderProcess = () => {
+    dispatch({
+      type: "begin-order-process",
+      // selectedItems: selectedItems,
+    });
+  };
+
+  const cancelOrderProcess = () => {
+    // setSelectedItems([]);
+    dispatch({
+      type: "cancel-order-process",
+    });
+  };
+
+  const orderRequested = () => {
+    const newArr = [...selectedItems];
+    dispatch({
+      type: "order-processing",
+      itemsPurchased: newArr,
+    });
+  };
+
+  const orderFailure = (errorMessage) => {
+    dispatch({
+      type: "order-failure",
+      error: errorMessage,
+    });
+  };
+
+  const orderSuccess = () => {
+    dispatch({
+      type: "order-success",
+    });
+    //empty the cart
+    setSelectedItems([]);
+  };
+
+  const afterPurchaseReset = () => {
+    dispatch({
+      type: "after-purchase-reset",
+    });
+  };
+
+  return (
+    <OrderContext.Provider
+      value={{
+        state,
+        actions: {
+          beginOrderProcess,
+          cancelOrderProcess,
+          orderRequested,
+          orderFailure,
+          orderSuccess,
+          afterPurchaseReset,
+        },
+        selectedItems,
+        setSelectedItems,
+        displayModal,
+        setDisplayModal,
+        lastOrder,
+        setLastOrder,
+      }}
+    >
+      {children}
+    </OrderContext.Provider>
+  );
+};
